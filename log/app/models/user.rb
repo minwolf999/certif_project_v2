c7cf56a3e99d7c2fe6180198a721187886309f3b
minwolf999 <minwolf.999@gmail.com>
2026-03-29 01:11:57 +0100

    Init App (#3)
    * add db config && add gems && add gitignore && add rubocop rules
    
    * install annotate gem
    
    * install cancancan
    
    * install devise with jwt strategy
    
    * fix pr return
    
    * add dotenv gem && re-run migration
    
    * fix pr return

---
+++ log/app/models/user.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  consumed_timestep      :integer
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  otp_required_for_login :boolean          default(FALSE), not null
+#  otp_secret             :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+class User < ApplicationRecord
+  devise :database_authenticatable, :two_factor_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: NullJwtStrategy
+end
