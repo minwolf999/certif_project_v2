c325e0efa053e7c9ae33c8fb10ad493fe4b49b44
anmarvin <minwolf.999@gmail.com>
2026-03-29 14:20:57 +0200

    add session controller

---
+++ log/app/controllers/session_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class SessionController < ApplicationController
+  def redirect_to_sign_in
+    redirect_to new_user_session_path
+  end
+
+  def send_otp
+    @user = User.find_by(email: otp_params[:email])
+    return redirect_to new_user_session_path, alert: I18n.t('devise.failure.invalid') unless @user&.valid_password?(otp_params[:password])
+
+    if @user.otp_already_give?
+      sign_in(@user)
+      redirect_to after_sign_in_path_for(@user)
+      return
+    end
+
+    UserMailer.with(user: @user).otp.deliver_later
+    flash.now[:notice] = I18n.t('devise.sessions.otp_sent')
+    @display_otp = true
+    @password = otp_params[:password]
+    render :new
+  end
+end
--- log/config/routes.rb
+++ log/config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  devise_scope :user do
+    get '/users/otp', to: 'sessions#redirect_to_sign_in'
+    post '/users/otp', to: 'sessions#send_otp', as: 'send_otp'
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
