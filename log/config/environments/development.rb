81eb4566a608df524af888b851e8a35c5b50a547
anmarvin <minwolf.999@gmail.com>
2026-04-05 21:06:19 +0200

    remove env set variables from enc yml file

--- log/config/environments/development.rb
+++ log/config/environments/development.rb
@@ -1,8 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  ENV['GOOGLE_OAUTH_CLIENT_ID'] = Rails.application.credentials.dig(:google_oauth_client_id_dev)
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
