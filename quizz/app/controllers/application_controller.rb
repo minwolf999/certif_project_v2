681d64b1decf76449e31cc2f59a9900b8760bc7c
anmarvin <minwolf.999@gmail.com>
2026-04-19 22:13:56 +0200

    initialize quizz app

---
+++ quizz/app/controllers/application_controller.rb
@@ -0,0 +1,29 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  before_action :authenticate_user!
+
+  private
+
+  def authenticate_user!
+    token = cookies['culture_g']
+    if token.nil?
+      return
+    end
+
+    payload, = JWT.decode(
+      token,
+      ENV['DEVISE_JWT_SECRET_KEY'] || Rails.application.credentials.devise_jwt_secret_key!,
+      true,
+      algorithm: 'HS256'
+    )
+
+    @current_user = payload['user']
+    @current_user = @current_user&.transform_keys(&:to_sym)
+  rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+    @current_user = nil
+  ensure
+    redirect_to '/users/sign_in' if @current_user.nil?
+  end
+end
