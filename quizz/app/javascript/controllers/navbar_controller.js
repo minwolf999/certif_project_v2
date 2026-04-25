eb833908fd1810643727483b6f870eadd3481b60
anmarvin <minwolf.999@gmail.com>
2026-04-26 00:08:55 +0200

    rework navbar visual

--- quizz/app/javascript/controllers/navbar_controller.js
+++ quizz/app/javascript/controllers/navbar_controller.js
@@ -1,22 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["navbar", "icon"]
+  static targets = ["navbar", "icon"];
 
-  connect() { this.is_open = true; }
+  connect() {
+    this.is_open = true;
+    this.dimension = this.navbarTarget.offsetWidth - this.iconTarget.offsetWidth - 4;
 
-  toggle() {
-    this.is_open ? this.open() : this.close();
-    this.is_open = !this.is_open;
+    this.close();
   }
 
-  open() {
-    this.navbarTarget.classList.replace('opacity-100', 'opacity-0');
+  toggle() {
+    console.log(this.dimension);
+    
+    this.is_open = !this.is_open;
+    this.is_open ? this.open() : this.close();
+  }
+
+  close() {
+    this.navbarTarget.style.left = `-${this.dimension}px`;
     this.iconTarget.classList.replace('rotate-0', 'rotate-180');
   }
   
-  close() {
-    this.navbarTarget.classList.replace('opacity-0', 'opacity-100');
+  open() {
+    this.navbarTarget.style.left = '0px';
     this.iconTarget.classList.replace('rotate-180', 'rotate-0');
   }
 }
